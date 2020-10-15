9dcd7b3d90a3877d466986c2d33f21ed260124f9
goodfed <gorozhanko@gmail.com>
2020-10-16 03:44:32 +1000

    new

--- convert_data.py
+++ convert_data.py
@@ -2,8 +2,8 @@ import json
 
 import data
 
-with open('static/goals.json', 'w') as g:
+with open('data/goals.json', 'w') as g:
     json.dump(data.goals, g)
 
-with open('static/teachers.json', 'w') as t:
+with open('data/teachers.json', 'w') as t:
     json.dump(data.teachers, t)
